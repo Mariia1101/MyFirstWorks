e3b3a5c57fc9a89f9d76e30bc1698eff5bf06140
Mariia <maryarabictranslator@ukr.net>
2020-12-02 21:37:04 +0200

    Calculator.myfirstwork

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MaryLiCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            string sign = (Console.ReadLine());
+
+            switch (sign)
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+                case "/":
+                        if (b == 0)
+                            Console.WriteLine("На 0 делить нельзя!");
+                        else
+                            Console.WriteLine(a / b);
+                        break;
+                case "*":
+                        if (b == 0)
+                            Console.WriteLine("На 0 множить нельзя!");
+                        else
+                            Console.WriteLine(a * b);
+                    break;
+                default:
+                    Console.WriteLine("Invalid sign!");
+                    break;
+
+            }
+        }
+    }
+}
